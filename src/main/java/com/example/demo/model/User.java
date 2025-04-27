595d85b883810a82307bdb420c9c33cb4f1b3b80
kashyap <kashyapkompella80@gmail.com>
2025-04-27 22:48:27 +0530

    Added Dockerfile and configured dynamic port

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,10 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Document(collection = "users")
 public class User {
     @Id
